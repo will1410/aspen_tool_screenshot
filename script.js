31fb7b9914dc5f19561b85bd6611c334a5da0095
George Williams <will1410@users.noreply.github.com>
2026-06-01 23:17:28 -0500

    Straighten up comments in script.js file

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
 
-  /* When the "Submit" button is clicked, all this happens */
+  /* When the "Submit" button is clicked, run this code */
     $("#accordion_generator").click(function () {
       event.preventDefault();
 
@@ -30,7 +30,7 @@ $(document).ready(function () {
           var screenshot_icon = '\n'
         }
 
-      /* Adds function to icons radio buttons */
+      /* Adds function to image width radio buttons */
         let widthType = $('input[name="panel_width"]:checked').val();
         var panel_width = '12'
 
@@ -48,7 +48,7 @@ $(document).ready(function () {
           var panel_width = '3'
         }
 
-      /* Adds function to icons image border */
+      /* Adds function to image border radio buttons */
         let imageBorder = $('input[name="image_options"]:checked').val();
         var image_border = ''
 
@@ -68,18 +68,15 @@ $(document).ready(function () {
         var seconds = ("0" + now.getSeconds()).slice(-2);
         var id_suffix = year + month + day + hour + minute + seconds;
 
-      /* Builds a single bootstrap accordion panel based around the inputs you've entered and outputs the raw HTML to the output textarea */
+      /* Builds a single bootstrap collapsible panel based around the inputs you've entered and outputs the raw HTML to the output textarea */
         $("#output").val(
-          '<div class="container">\n' +
-          '<span style="display: none;">=== BEGIN ' +
-          id_suffix +
+          '<div class="container">\n' + 
+          '<span style="display: none;">=== BEGIN ' + 
+          id_suffix + 
           '===</span>\n' +
           '  <div class="row">\n' +
           '    <div class="col-md-' + panel_width + '">\n' +
-
-
           '      <div>\n' +
-         
           '        <div class="panel panel-default" style="border: none; box-shadow: none;">\n' +
           '          <div class="panel-heading" role="tab" id="heading-' +
           id_suffix +
@@ -120,7 +117,7 @@ $(document).ready(function () {
         );
     });
 
-    /* Copies output to clipboard */
+    /* Adds function to the "Copy to clipboard" button */
       $("#copy_to_clipboard").click(function () {
         event.preventDefault();
         var copy_output = $("#output").val().trim();
